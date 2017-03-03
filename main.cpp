5c7884856f8a360307b663e7227427ce1a459864
eduescobar13 <eduescal13@gmail.com>
2017-03-03 11:57:32 +0000

    Añadido el fichero main.cpp.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//|-----------------------------------------------------------------------|
+//| Autor       : EDUARDO ESCOBAR ALBERTO                                 |
+//| Versión     : 07/03/2017                                              |
+//| Comentarios : Diseño y Análisis de Algoritmos                         |
+//|               Escuela Superior de Ingeniería y Tecnología             |
+//|               Universidad de La Laguna                                |  
+//|                                                                       |
+//| Compilación : g++ -g main.cpp -o TorresHanoi                          |                                                         
+//|-----------------------------------------------------------------------|
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) { 
+
+	system ("clear");
+
+	// DECLARACIÓN DE LOS OBJETOS.
+	UnidadEntrada *unidadEntrada;
+	UnidadMemoria *unidadMemoria;
+	UnidadALC	  *unidadALC;
+	UnidadSalida  *unidadSalida;
+
+	// DECLARACIÓN DE VARIABLES.
+	//char debugChar = *argv[4]; // Obtenemos el valor del debug.
+	//int debug = debugChar - '0'; // Convertimos dicho valor a un entero.
+
+	cout << "  ___|PRÁCTICA 2: DISEÑO Y ANÁLISIS DE ALGORITMOS|___" << endl;
+	cout << "------------------- TORRES DE HANOI -------------------" << endl;
+
+	return(0); 
+}
