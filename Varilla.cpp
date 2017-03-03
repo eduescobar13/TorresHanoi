b952dae1a6345ea66d8aa2de7fde50c761f4bacf
eduescobar13 <eduescal13@gmail.com>
2017-03-03 12:24:21 +0000

    Añadidos los ficheros de la clase Varilla.
    Clase para la implementación de Varillas de Las Torres de Hanoi.

---
+++ Varilla.cpp
@@ -0,0 +1,48 @@
+#include "Varilla.hpp" // Incluimos el fichero Varilla.hpp con las declaracion de nuestra clase.
+
+Varilla::Varilla(void): // Constructor por defecto.
+    numeroDiscosMaximo(0),
+    numeroDiscosActual(0),
+    vectorVarilla()
+{}
+
+Varilla::Varilla(int tamanoVarilla): // Constructor.
+    numeroDiscosMaximo(tamanoVarilla),
+    numeroDiscosActual(0), // Inicialmente no habrá disco en la varilla.
+    vectorVarilla()
+{
+	vectorVarilla.resize(numeroDiscosMaximo); // Redimensionamos el vector.
+}
+
+Varilla::~Varilla(void) { // Destructor.
+	vectorVarilla.clear(); // Vaciamos el vectorVarilla.
+	numeroDiscosMaximo = 0;
+	numeroDiscosActual = 0;
+}
+
+//----------------> MÉTODOS GETTER Y SETTER DE LA CLASE.
+
+int Varilla::getNumeroDiscosMaximo() { // Método getter del atributo numeroDiscosMaximo.
+	return numeroDiscosMaximo;
+}
+
+void Varilla::setNumeroDiscosMaximo(numeroDiscosMaximo) { // Método setter del atributo numeroDiscosMaximo.
+	this->numeroDiscosMaximo = numeroDiscosMaximo;
+}
+
+int Varilla::getNumeroDiscosActual() { // Método getter del atributo numeroDiscosActual.
+	return numeroDiscosActual;
+}
+
+void Varilla::setNumeroDiscosActual(numeroDiscosActual) { // Método setter del atributo numeroDiscosActual.
+	this->numeroDiscosActual = numeroDiscosActual;
+}
+
+vector<int> Varilla::getVectorVarilla() { // Método getter del atributo vectorVarilla.
+	return cintaEntrada;
+}
+
+void Varilla::setVectorVarilla(vector<int> vectorVarilla) { // Método setter del atributo vectorVarilla.
+	this->vectorVarilla = cintaEntrada;
+}
+
---
+++ Varilla.hpp
@@ -0,0 +1,30 @@
+#pragma once // Usado para no crear conflictos entre include similares.
+
+#include <iostream> 
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Varilla { // Clase para la implementación de varillas de Las Torres de Hanoi.
+
+    private: // Atributos privados de la clase.
+
+        int numeroDiscosMaximo; // Atributo para determinar el número de discos que soporta la varilla.
+        int numeroDiscosActual; // Atributo para determinar el número de discos que hay actualmente en la varilla.
+        vector<int> vectorVarilla; // Vector que hará de varilla.
+
+    public: // Métodos privados de la clase.
+
+    	Varilla(void); // Constructor por defecto.
+        Varilla(int numeroDiscos); // Constructor.
+        ~Varilla(void); // Destructor.
+
+        int getNumeroDiscosMaximo(); // Método getter del atributo numeroDiscosMaximo.
+        void setNumeroDiscosMaximo(numeroDiscosMaximo); // Método setter del atributo numeroDiscosMaximo.
+        int getNumeroDiscosActual(); // Método getter del atributo numeroDiscosActual.
+        void setNumeroDiscosActual(numeroDiscosActual); // Método setter del atributo numeroDiscosActual.
+        vector<int> getVectorVarilla(); // Método getter del atributo vectorVarilla.
+        void setVectorVarilla(vector<int> vectorVarilla); // Método setter del atributo vectorVarilla.
+
+};
